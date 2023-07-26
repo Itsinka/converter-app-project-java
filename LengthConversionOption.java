87303f6a2dcaaaac420a9dbbb1dbac5d8afdc57b
Inkarim <114926067+Itsinka@users.noreply.github.com>
2023-07-27 00:09:14 +0600

    Add files via upload

---
+++ LengthConversionOption.java
@@ -0,0 +1,11 @@
+public class LengthConversionOption {
+    private double meters;
+
+    public double getMeters() {
+        return meters;
+    }
+
+    public void setMeters(double meters) {
+        this.meters = meters;
+    }
+}
